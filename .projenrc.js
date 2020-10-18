d75a1a49811ec34a031da56d8264307ec69f5c8b
Niko Virtala <nikovirtala@users.noreply.github.com>
2020-10-18 22:19:23 +0300

    chore: update dependencies (#17)

--- .projenrc.js
+++ .projenrc.js
@@ -1,7 +1,7 @@
 const { AwsCdkTypeScriptApp } = require('projen');
 
 const project = new AwsCdkTypeScriptApp({
-  cdkVersion: '1.65.0',
+  cdkVersion: '1.68.0',
   name: 'cdk-preview-environments',
   repository: 'https://github.com/nikovirtala/cdk-preview-environments.git',
   license: 'MIT',
