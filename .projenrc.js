29f50e933e5f27e8768e17431f419d050c31e4bc
Niko Virtala <niko.virtala@hey.com>
2021-01-26 11:26:26 +0200

    chore: bump cdk version.

--- .projenrc.js
+++ .projenrc.js
@@ -1,7 +1,7 @@
 const { AwsCdkTypeScriptApp } = require('projen');
 
 const project = new AwsCdkTypeScriptApp({
-  cdkVersion: '1.85.0',
+  cdkVersion: '1.86.0',
   name: 'cdk-preview-environments',
   repository: 'https://github.com/nikovirtala/cdk-preview-environments.git',
   license: 'MIT',
