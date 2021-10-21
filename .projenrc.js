8f65b58b566cc4b7d3b64f486dead1d097a446ed
Niko Virtala <niko.virtala@hey.com>
2021-10-21 15:42:35 +0300

    chore(deps): update all dependencies.

--- .projenrc.js
+++ .projenrc.js
@@ -1,7 +1,7 @@
 const { AwsCdkTypeScriptApp } = require('projen');
 
 const project = new AwsCdkTypeScriptApp({
-  cdkVersion: '1.97.0',
+  cdkVersion: '1.128.0',
   name: 'cdk-preview-environments',
   repository: 'https://github.com/nikovirtala/cdk-preview-environments.git',
   license: 'MIT',
@@ -10,12 +10,27 @@ const project = new AwsCdkTypeScriptApp({
   authorEmail: 'niko.virtala@hey.com',
   devDeps: ['prettier'],
   cdkDependencies: ['@aws-cdk/aws-ecs', '@aws-cdk-containers/ecs-service-extensions'],
+  depsUpgradeOptions: {
+    ignoreProjen: false,
+    workflowOptions: {
+      labels: ['auto-approve', 'auto-merge'],
+      secret: 'AUTOMATION_TOKEN',
+    },
+  },
+  autoApproveOptions: {
+    secret: 'GITHUB_TOKEN',
+    allowedUsernames: ['nikovirtala'],
+  },
   context: {
     '@aws-cdk/core:newStyleStackSynthesis': 'true',
     'aws-cdk:enableDiffNoFail': 'true',
   },
   buildWorkflow: true,
   codeCov: false,
+  eslint: true,
+  eslintOptions: {
+    prettier: true,
+  },
   jest: false,
   mergify: true,
   pullRequestTemplate: false,
