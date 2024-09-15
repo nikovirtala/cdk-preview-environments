5c0e02698068514d0d50e47e8ebf4e4ffa38d780
Niko Virtala <niko.virtala@hey.com>
2024-09-15 13:12:32 +0300

    chore(deps): upgrade dependencies

---
+++ .projenrc.ts
@@ -0,0 +1,34 @@
+import { awscdk } from "projen";
+
+const project = new awscdk.AwsCdkTypeScriptApp({
+  cdkVersion: "2.158.0",
+  name: "cdk-preview-environments",
+  repository: "https://github.com/nikovirtala/cdk-preview-environments.git",
+  license: "MIT",
+  licensed: true,
+  authorName: "Niko Virtala",
+  authorEmail: "niko.virtala@hey.com",
+  devDeps: ["prettier"],
+  deps: ["@aws-cdk-containers/ecs-service-extensions@^2"],
+  depsUpgradeOptions: {
+    workflowOptions: {
+      labels: ["auto-approve", "auto-merge"],
+    },
+  },
+  autoApproveOptions: {
+    secret: "GITHUB_TOKEN",
+    allowedUsernames: ["nikovirtala"],
+  },
+  buildWorkflow: true,
+  codeCov: false,
+  eslint: true,
+  prettier: true,
+  jest: false,
+  mergify: true,
+  pullRequestTemplate: false,
+  defaultReleaseBranch: "main",
+  typescriptVersion: "5.5.4",
+  projenrcTs: true,
+});
+
+project.synth();
