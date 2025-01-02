309c983d5e7cb6fb8b5b40ea24a9ef47b3799f6a
Niko Virtala <nikovirtala@users.noreply.github.com>
2025-01-02 16:46:51 +0200

    feat: annual update (#966)
    - **refactor(app): service and environment declaration**
    - **refactor(lint): declare workflow using projen**
    - **refactor: sort project properties**

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,37 +1,41 @@
 import { awscdk } from "projen";
-import { DeploymentWorkflow, DestroyWorkflow } from "./src/workflows";
+import {
+  DeploymentWorkflow,
+  DestroyWorkflow,
+  LintWorkflow,
+} from "./src/workflows";
 
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: "2.173.4",
-  name: "cdk-preview-environments",
-  repository: "https://github.com/nikovirtala/cdk-preview-environments.git",
-  license: "MIT",
-  licensed: true,
-  authorName: "Niko Virtala",
+  autoApproveOptions: {
+    allowedUsernames: ["nikovirtala"],
+    secret: "GITHUB_TOKEN",
+  },
   authorEmail: "niko.virtala@hey.com",
-  devDeps: ["prettier"],
+  authorName: "Niko Virtala",
+  buildWorkflow: true,
+  cdkVersion: "2.173.4",
+  codeCov: false,
+  defaultReleaseBranch: "main",
   deps: ["@aws-cdk-containers/ecs-service-extensions@^2"],
   depsUpgradeOptions: {
     workflowOptions: {
       labels: ["auto-approve", "auto-merge"],
     },
   },
-  autoApproveOptions: {
-    secret: "GITHUB_TOKEN",
-    allowedUsernames: ["nikovirtala"],
-  },
-  buildWorkflow: true,
-  codeCov: false,
+  devDeps: ["prettier"],
   eslint: true,
-  prettier: true,
-  jest: false,
-  mergify: true,
-  pullRequestTemplate: false,
-  defaultReleaseBranch: "main",
-  typescriptVersion: "5.7.2",
-  projenrcTs: true,
-  minNodeVersion: "22.12.0",
   github: true,
+  jest: false,
+  license: "MIT",
+  licensed: true,
+  mergify: true,
+  minNodeVersion: "22.12.0",
+  name: "cdk-preview-environments",
+  prettier: true,
+  projenrcTs: true,
+  pullRequestTemplate: false,
+  repository: "https://github.com/nikovirtala/cdk-preview-environments.git",
+  typescriptVersion: "5.7.2",
 });
 
 project.eslint?.addOverride({
@@ -43,5 +47,6 @@ project.eslint?.addOverride({
 
 new DeploymentWorkflow(project);
 new DestroyWorkflow(project);
+new LintWorkflow(project);
 
 project.synth();
