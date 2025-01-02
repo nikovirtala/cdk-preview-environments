e190a9ecd77c62b88ebe8eac03455529153ce80c
Niko Virtala <niko.virtala@hey.com>
2025-01-02 16:03:44 +0200

    refactor(app): service and environment declaration

--- src/main.ts
+++ src/main.ts
@@ -5,35 +5,33 @@ import {
   Service,
   ServiceDescription,
 } from "@aws-cdk-containers/ecs-service-extensions";
-import { App, Stack } from "aws-cdk-lib";
-import { ContainerImage } from "aws-cdk-lib/aws-ecs";
+import { App, Stack, aws_ecs } from "aws-cdk-lib";
 import { Construct } from "constructs";
 
 export class MyStack extends Stack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    const environment = new Environment(this, stage);
-
-    const serviceDescription = new ServiceDescription();
-    serviceDescription.add(
-      new Container({
-        cpu: 256,
-        memoryMiB: 512,
-        trafficPort: 8080,
-        image: ContainerImage.fromRegistry("nikovirtala/honk"),
-      })
-    );
-    serviceDescription.add(new HttpLoadBalancerExtension());
-
     new Service(this, "service", {
-      environment: environment,
-      serviceDescription: serviceDescription,
+      environment: new Environment(this, stage),
+      serviceDescription: new ServiceDescription()
+        .add(
+          new Container({
+            cpu: 256,
+            memoryMiB: 512,
+            trafficPort: 8080,
+            image: aws_ecs.ContainerImage.fromRegistry("nikovirtala/honk"),
+          })
+        )
+        .add(new HttpLoadBalancerExtension()),
     });
   }
 }
 
 const app = new App();
-const stage = app.node.tryGetContext("stage") || "default";
+
+const stage = app.node.tryGetContext("stage") ?? "default";
+
 new MyStack(app, "cdk-preview-environments-" + stage);
+
 app.synth();
